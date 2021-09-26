9a332813b7050177d01b0df6c50234075e10e165
SecorD0 <vovan6573@gmail.com>
2021-09-26 15:37:34 +0300

    Updates the node

--- updater.sh
+++ updater.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
-# $1 - Solana version
-if [[ $1 ]]; then
-	solana_version=$1
-else
-	echo -e "\033[0;31mYou didn't specify a version\e[0m"
-	exit 1
-fi
+# Default variables
+sudo apt install jq -y &>/dev/null
+solana_version=`curl -s https://api.github.com/repos/solana-labs/solana/releases/latest | jq -r ".tag_name" | sed "s%v%%g"`
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script unpdates Solana node"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help             show the help page"
+		echo -e "  -v, --version VERSION  Solana node VERSION to update (default is ${C_LGn}${solana_version}${RES})"
+		echo
+		echo -e "${C_LGn}Useful URLs${RES}:"
+		echo -e "https://github.com/SecorD0/Solana/blob/main/updater.sh - script URL"
+		echo -e "https://t.me/letskynode — node Community"
+		echo
+		return 0
+		;;
+	-v*|--version*)
+		if ! grep -q "=" <<< "$1"; then shift; fi
+		solana_version=`option_value "$1"`
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
+done
+# Functions
+printf_n(){ printf "$1\n" "${@:2}"; }
+# Actions
 if [ ! -f /etc/systemd/system/sstd.service ]; then
 	sudo tee <<EOF >/dev/null /etc/systemd/system/sstd.service
 [Unit]
@@ -27,7 +58,7 @@ EOF
 	sudo systemctl daemon-reload
 fi
 if ! solana --version | grep -q $solana_version; then
-	solana-install init "v$solana_version"
+	solana-install init "v${solana_version}"
 	solana-validator --ledger $HOME/solana/ledger/ wait-for-restart-window
 	sudo systemctl stop solana
 	sudo systemctl restart sstd
