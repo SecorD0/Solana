f4e519e8f7eb3478839d7a94c46fcbcbced70cf1
SecorD0 <vovan6573@gmail.com>
2021-09-30 23:39:21 +0300

    Restarts the node when a critical percentage of memory usage is reached

--- auto_restarter.sh
+++ auto_restarter.sh
@@ -27,7 +27,7 @@ while test $# -gt 0; do
 		echo -e "https://github.com/SecorD0/Solana/blob/main/auto_restarter.sh - script URL"
 		echo -e "https://t.me/letskynode — node Community"
 		echo
-		return 0; exit 0
+		return 0 2>/dev/null; exit 0
 		;;
 	-cp*|--crit-percent*)
 		if ! grep -q "=" <<< "$1"; then shift; fi
