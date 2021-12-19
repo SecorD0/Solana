9fb18e3da3d1da04a0364286d64cf2fb68bbb273
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-12-19 23:55:09 +0300

    Performs many functions

--- multi_tool.sh
+++ multi_tool.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Default variables
-type="install"
+function="install"
+
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
@@ -9,16 +10,18 @@ while test $# -gt 0; do
 	-h|--help)
 		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 		echo
-		echo -e "${C_LGn}Functionality${RES}: the script has a set of functions to work with the Solana node"
+		echo -e "${C_LGn}Functionality${RES}: the script performs many actions related to a Solana node"
 		echo
 		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
 		echo
 		echo -e "${C_LGn}Options${RES}:"
-		echo -e "  -h, --help       show the help page"
+		echo -e "  -h, --help  show the help page"
 		echo
 		echo -e "${C_LGn}Useful URLs${RES}:"
 		echo -e "https://github.com/SecorD0/Solana/blob/main/multi_tool.sh - script URL"
+		echo -e "https://teletype.in/@letskynode/Solana_part1_general — series of Russian-language articles on the Solana node"
 		echo -e "https://t.me/letskynode — node Community"
+		echo -e "https://teletype.in/@letskynode — guides and articles"
 		echo
 		return 0 2>/dev/null; exit 0
 		;;
@@ -27,18 +30,19 @@ while test $# -gt 0; do
 		;;
 	esac
 done
+
+# Functions
+printf_n(){ printf "$1\n" "${@:2}"; }
+install() {
+	sudo apt update
+	sudo apt upgrade -y
+	local solana_version=`. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/parsers/xpath.sh) -x "normalize-space(/html/body/main/div[5]/div[2]/div/div/div/div[3]/div/text())" -u "https://www.validators.app/cluster-stats?locale=en&network=testnet"`
+	. <(wget -qO- "https://release.solana.com/v${solana_version}/install")
+	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n "PATH" -v "/root/.local/share/solana/install/active_release/bin:$PATH"
+	echo -e "${C_LGn}Done!${RES}"
+}
+
 # Actions
-sudo apt install wget bc -y &>/dev/null
+sudo apt install wget -y &>/dev/null
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
-if [ "$type" = "" ]; then
-	echo
-else
-	if ! solana --version &>/dev/null; then
-		sudo apt update
-		sudo apt upgrade -y
-		solana_version=`. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/parsers/xpath.sh) -x "normalize-space(/html/body/main/div[5]/div[2]/div/div/div/div[3]/div/text())" -u https://www.validators.app/cluster-stats/testnet`
-		. <(wget -qO- "https://release.solana.com/v${solana_version}/install")
-		. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n "PATH" -v "/root/.local/share/solana/install/active_release/bin:$PATH"
-	fi
-fi
-echo -e "${C_LGn}Done!${RES}"
+$function
